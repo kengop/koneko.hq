541d73d1acd360fc94b2b0017d4fd01650fe699c
Nijiko Kyutoku <nijiko@NijikonoMacBook-Air.local>
2018-10-06 11:33:18 +0900

    Change NavigationActivity

--- app/src/main/java/com/hq/koneko/myapplication/NavigationActivity.java
+++ app/src/main/java/com/hq/koneko/myapplication/NavigationActivity.java
@@ -80,13 +80,9 @@ public class NavigationActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_googlescholar) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_trashbox) {
 
         } else if (id == R.id.nav_share) {
 
